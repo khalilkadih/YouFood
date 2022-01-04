5413080a28c20e9433d3e3824e0a92ca14a2799f
Safia MAANI <saf.maani@gmail.com>
2022-01-04 22:30:40 +0100

    ajout du fichier JS

---
+++ log in/js_log.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+function openfff(){
+    if(document.getElementById("in").value == "user")
+    {
+        window.open("index.html");
+    }
+    else
+    if(document.getElementById("in").value == "admin")
+    {
+        window.open("index_10.html");
+    }
+    else
+    {
+        window.alert("wrong XD");
+    }
+}
