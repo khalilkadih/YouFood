ef2f8608f0b68c0339d1a4e0986289817d2e2372
khalil el kadih <khalil94elkadih@gmail.com>
2022-01-09 21:28:04 +0000

    final changes

--- gestion chefs/Menu/chefs.js
+++
@@ -1,9 +0,0 @@
-function openit(){
-    document.getElementById("cantainer").style.display = "block";
-}
-
-function closeit(){
-    
-    document.getElementById("cantainer").style.display = "none";
-
-}
--- logINpage/js.js
+++ logINpage/js.js
@@ -1,15 +1,15 @@
-function openfff(){
-    if(document.getElementById("in").value == "user")
-    {
-        window.open("index.html");
-    }
-    else
-    if(document.getElementById("in").value == "admin")
-    {
-        window.open("index_10.html");
-    }
-    else
-    {
-        window.alert("wrong XD");
+function CheckUser() {
+
+    if (document.getElementById("in").value == "user") {
+
+        window.open("../YouFood-Badr/index.html");
+    } else
+    if (document.getElementById("in").value == "admin") {
+        window.open("../admin/admin.html");
+    } else
+    if (document.getElementById("in").value == "chef") {
+        window.open("../YouFood-Mourad/Menu/index.html");
+    } else {
+        window.alert("wrong !!!! Verifie votre user name (user ,chef ,admin)");
     }
 }
